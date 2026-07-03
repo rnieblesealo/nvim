f410677795eb3df596d31c6dc3e5629e23c1dd33
Rafael Niebles <rniebles@proton.me>
2026-07-03 15:25:25 -0400

    floating nvimtree + light theme

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,6 +7,7 @@ local function set_mappings() -- sets nvimtree mappings for any buffer
   }
 
   vim.keymap.set("n", "<leader>n", api.tree.toggle, opts) -- open with leader + n
+  vim.keymap.set("n", "q", api.tree.close, opts)          -- close with q
 end
 
 return {
@@ -17,7 +18,29 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup()
+    require("nvim-tree").setup({
+      view = {
+        float = {
+          enable = true,
+          open_win_config = function()
+            local editor_width = vim.o.columns
+            local editor_height = vim.o.lines
+
+            local width = math.floor(editor_width * 0.7)
+            local height = 20
+
+            return {
+              relative = "editor",
+              border = "rounded",
+              width = width,
+              height = height,
+              row = math.floor((editor_height - height) / 2) - 1,
+              col = math.floor((editor_width - width) / 2),
+            }
+          end,
+        },
+      },
+    })
     set_mappings()
   end,
 }
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,78 +1,9 @@
 return {
-  'maxmx03/solarized.nvim',
-  lazy = false,
-  priority = 1000,
-  opts = {
-    transparent = {
-      enabled = false,
-      pmenu = true,
-      normal = true,
-      normalfloat = true,
-      neotree = true,
-      nvimtree = true,
-      whichkey = true,
-      telescope = true,
-      lazy = true,
-    },
-    on_highlights = nil,
-    on_colors = nil,
-    palette = 'solarized', -- solarized (default) | selenized
-    variant = 'summer',    -- "spring" | "summer" | "autumn" | "winter" (default)
-    error_lens = {
-      text = false,
-      symbol = false,
-    },
-    styles = {
-      enabled = true,
-      types = {},
-      functions = {},
-      parameters = {},
-      comments = {},
-      strings = {},
-      keywords = {},
-      variables = {},
-      constants = {},
-    },
-    plugins = {
-      treesitter = true,
-      lspconfig = true,
-      navic = true,
-      cmp = true,
-      indentblankline = true,
-      indentmini = true,
-      neotree = true,
-      nvimtree = true,
-      whichkey = true,
-      dashboard = true,
-      gitsigns = true,
-      telescope = true,
-      noice = true,
-      hop = true,
-      ministatusline = true,
-      minitabline = true,
-      ministarter = true,
-      minicursorword = true,
-      notify = true,
-      rainbowdelimiters = true,
-      bufferline = true,
-      lazy = true,
-      rendermarkdown = true,
-      ale = true,
-      coc = true,
-      leap = true,
-      alpha = true,
-      yanky = true,
-      gitgutter = true,
-      mason = true,
-      flash = true,
-    },
-  },
-  config = function(_, opts)
-    vim.o.termguicolors = true
-    vim.o.background = 'dark'
-
-    require('solarized').setup(opts)
-
-    vim.cmd.colorscheme 'solarized'
+  'projekt0n/github-nvim-theme',
+  name = 'github-theme',
+  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    vim.cmd('colorscheme github_light')
   end,
 }
